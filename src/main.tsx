2165e68e98974c4e2a930ca6340ef021711e4998
AndresRuiu <andresruiu@gmail.com>
2024-12-03 00:55:03 -0300

    x

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+injectSpeedInsights();
 inject();
 
 createRoot(document.getElementById('root')!).render(
